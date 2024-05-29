224fae7e5134621bc4977d9d6f08249646d9b862
desk7111 <kdh7111@gmail.com>
2024-05-29 19:41:56 +0900

    두 번째 커밋

--- Home111/Form1.cs
+++ Home111/Form1.cs
@@ -16,5 +16,15 @@ namespace Home111
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hi");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Bye");
+        }
     }
 }
